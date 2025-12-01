8834fcf2372563f2fd9e518d7c4319e205b2d557
Tristan Basri <tristanbasri@Tristans-MacBook-Pro.local>
2025-12-01 17:58:28 +0000

    Added haskell DAY1 solution

--- Haskell/DAY1.hs
+++ Haskell/DAY1.hs
@@ -1,32 +1,44 @@
--- module Haskell.DAY1 where
--- import Data.Foldable (foldl')
--- -- This code is unsafe/could err!
--- -- (only meant to be run on a well-formed dial text file)
+import qualified Data.Map as Map
+import Data.Map ((!), Map)
+import Control.Monad.State
 
--- main :: IO()
--- main = do
---     file <- readFile "External_Files/dials.txt"
---     print $ parseDials file
+main :: IO()
+main = do
+    file <- readFile "External_Files/dials.txt"
+    let file' = lines file
+    print $ "PART 1: " ++ show (evalState (password1 file') (0,50))
+    print $ "PART 2: " ++ show (evalState (password2 file') (0,50))
 
--- -----
+--              pwd pos
+type PWState = (Int,Int)
 
--- type State = (Int,Int,Int)
+drns :: Map.Map Char (Int, Int -> Int -> Bool)
+drns = Map.fromList [
+    ('L', (-1, (>))), 
+    ('R', ( 1, (<)))
+    ]
 
--- getAns :: State -> (Int,Int)
--- getAns (_,p1,p2) = (p1,p2)
+password1 :: [String] -> State PWState Int
+password1 [] = gets fst
+password1 ((d:s):ls) = do
+    (pwd, pos) <- get
+    let pos' = (pos +  fst (drns ! d) * read s) `mod` 100
+    let pwd' = if   pos' == 0 
+               then pwd + 1 
+               else pwd 
+    put (pwd',pos')
+    password1 ls
 
+password2 :: [String] -> State PWState Int
+password2 [] = gets fst
+password2 ((d:s):ls) = do
+    (pwd, pos) <- get
+    let (cycles,step) = read s `divMod` 100
+    let (dir,op) = drns ! d
 
-
--- -----
-
--- solve :: [Int] -> (Int, Int)
--- solve = getAns . foldl' solve' (50,0,0)
---     where solve' :: State -> Int -> State
-
--- parseDials :: String -> [Int]
--- parseDials = map parseDial . lines
---     where parseDial :: String -> Int
---           parseDial ('L':step) = read step
---           parseDial ('R':step) = -read step
-
--- WIP
+    let pos' = (pos + (dir * step)) `mod` 100
+    let pwd' = cycles + if   pos' == 0 || pos' `op` pos && pos /= 0
+                        then pwd + 1 
+                        else pwd
+    put (pwd',pos')
+    password2 ls
