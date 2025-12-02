faceb21d8848dbbaa615dcf7c50f854be2106fe9
Tristan Basri <tristanbasri@Tristans-MacBook-Pro.local>
2025-12-02 10:04:57 +0000

    DAY2 Python

---
+++ Python/DAY2.py
@@ -0,0 +1,22 @@
+import re
+
+with open('External_Files/ids.txt') as f:
+    ranges = f.read().split(',')
+f.close()
+
+# alternative (easier imo) regex solution
+pattern1 = re.compile(r'^(\d+)\1$')
+pattern2 = re.compile(r'^(\d+)\1+$')
+def find(x):
+    a, b = x
+    a, b = int(a), int(b)
+    s1 = s2 = 0
+    for k in range(a,b+1):
+        s = str(k)
+        if pattern1.match(s): s1 += k
+        if pattern2.match(s): s2 += k
+    return (s1,s2)
+
+rs = [find(x.split('-')) for x in ranges]
+ans = list(map(sum,zip(*rs)))
+print(ans) # PART 1: 40214376723, PART 2: 50793864718
