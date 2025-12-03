d5b3649df00acf2b94f5d4a85870c80b5c3598b1
Tristan Basri <tristanbasri@Tristans-MacBook-Pro.local>
2025-12-03 18:41:36 +0000

    Haskell DAY3; removed head/tail

--- Haskell/DAY3.hs
+++ Haskell/DAY3.hs
@@ -9,16 +9,21 @@ main = do
     let banks = map (map digitToInt) $ lines file
     print $ maxBanks 2 banks  -- PART 1: 17430
     print $ maxBanks 12 banks -- PART 2: 171975854269367
-    
+
 type StkState = ([Int], Int)
 
 maxBanks :: Int -> [[Int]] -> Int
-maxBanks k = sum . map (\b -> combineDigits $ take k $ evalState (maxBank b) ([],length b - k))
-    where combineDigits = read . concatMap show
+maxBanks k = sum . map (\b -> 
+    (read . concatMap show) $ 
+    take k                  $ 
+    evalState (maxBank b) ([],length b - k))
 
 maxBank :: [Int] -> State StkState [Int]
 maxBank [] = gets $ reverse . fst
 maxBank bank@(b:bs) = do
     (stk,canPop) <- get
-    if null stk || canPop <= 0 || b <= head stk then put (b:stk, canPop) >> maxBank bs
-    else put (tail stk, canPop-1) >> maxBank bank
+    case stk of
+        (d:ds) -> if canPop <= 0 || b <= d 
+                  then put (b:stk, canPop) >> maxBank bs
+                  else put (ds, canPop-1) >> maxBank bank
+        _ -> put (b:stk, canPop) >> maxBank bs
