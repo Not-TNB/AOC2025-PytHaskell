886d5fa09bfc1fbeccf0eee5a5e39879bc16b653
Tristan Basri <tristanbasri@Tristans-MacBook-Pro.local>
2025-12-03 18:52:53 +0000

    Improved Haskell Day3 slightly

--- Haskell/DAY3.hs
+++ Haskell/DAY3.hs
@@ -7,7 +7,7 @@ main :: IO()
 main = do
     file <- readFile "../External_Files/day3.txt"
     let banks = map (map digitToInt) $ lines file
-    print $ maxBanks 2 banks  -- PART 1: 17430
+    print $ maxBanks  2 banks -- PART 1: 17430
     print $ maxBanks 12 banks -- PART 2: 171975854269367
 
 type StkState = ([Int], Int)
@@ -22,8 +22,9 @@ maxBank :: [Int] -> State StkState [Int]
 maxBank [] = gets $ reverse . fst
 maxBank bank@(b:bs) = do
     (stk,canPop) <- get
-    case stk of
-        (d:ds) -> if canPop <= 0 || b <= d 
-                  then put (b:stk, canPop) >> maxBank bs
-                  else put (ds, canPop-1) >> maxBank bank
-        _ -> put (b:stk, canPop) >> maxBank bs
+    let next = put(b:stk, canPop) >> maxBank bs in
+        case stk of
+            (d:ds) -> if canPop <= 0 || b <= d 
+                    then next
+                    else put (ds, canPop-1) >> maxBank bank
+            _ -> next
