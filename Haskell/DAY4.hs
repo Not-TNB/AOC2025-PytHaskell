9ad41ac379dee7775558b5829b29f62c1f0fc0fa
Tristan Basri <tristanbasri@Tristans-MacBook-Pro.local>
2025-12-06 02:01:45 +0000

    Haskell D5

--- Haskell/DAY4.hs
+++ Haskell/DAY4.hs
@@ -6,31 +6,23 @@ import Utils
 
 main :: IO ()
 main = do
-    file <- readFile "../External_Files/day4.txt"
-    let (n,m,gridArr) = setupGrid file
-        initState = (0, gridArr)
-    print $ evalState (removePaperArray n m gridArr) initState
-    print $ evalState (part2Array       n m gridArr) initState
+    grid <- setupGrid <$> readFile "../External_Files/day4.txt"
+    print $ evalState (removePaperArray grid) (0,grid)
+    print $ evalState (part2Array       grid) (0,grid)
 
-type GridState = (Int, GridArray)  -- (total removed, current grid)
+type GridState = (Int, GridArray)
 
-toRemoveIdxs :: Int -> Int -> GridArray -> [Int]
-toRemoveIdxs n m grid =
-    [ idx | i <- [0..n-1], j <- [0..m-1], let idx=i*m+j
-    , grid ! idx == '@', length [ () | (rr,cc) <- liftA2 (,) [i-1,i,i+1] [j-1,j,j+1]
-                       , rr >= 0 && rr < n && cc >= 0 && cc < m
-                       , grid ! (rr*m+cc) == '@'] <= 4 ]
+toRemoveIdxs :: GridArray -> [(Int,Int)]
+toRemoveIdxs grid = [pos | pos <- coords grid, grid!pos == '@' && ngbrs grid (=='@') pos <= 4]
 
-removePaperArray :: Int -> Int -> GridArray -> State GridState Int
-removePaperArray n m grid = do
-    (tot, _) <- get
-    let removeIdx = toRemoveIdxs n m grid
-        removed = length removeIdx
-    put (tot + removed, grid // map (, '.') removeIdx)
+removePaperArray ::  GridArray -> State GridState Int
+removePaperArray grid = do
+    let removeList = toRemoveIdxs grid
+        removed    = length removeList
+    modify (\(tot,g) -> (tot + removed, g // map (,'.') removeList))
     return removed
 
-part2Array :: Int -> Int -> GridArray -> State GridState Int
-part2Array n m grid = do
-    removed <- removePaperArray n m grid
-    if removed == 0 then get >>= \(tot, _)     -> return tot
-    else                 get >>= \(_, gridNew) -> part2Array n m gridNew
+part2Array :: GridArray -> State GridState Int
+part2Array grid = do
+    removed <- removePaperArray grid
+    if removed == 0 then gets fst else gets snd >>= part2Array
--- Haskell/Utils.hs
+++ Haskell/Utils.hs
@@ -1,12 +1,20 @@
 module Utils where
 
 import Data.Array
-import Data.List.Split
 
-type GridArray = Array Int Char
-setupGrid :: String -> (Int, Int, GridArray)
-setupGrid grid = (n,m,gridArr)
-    where grid2D = splitOn "\n" grid
-          n = length grid2D
-          m = length (head grid2D)
-          gridArr = listArray (0, n*m-1) $ concat grid2D
+type GridArray = Array (Int,Int) Char
+
+setupGrid :: String -> GridArray
+setupGrid str = array ((0,0),(n-1,m-1)) $ zip (liftA2 (,) [0..n-1] [0..m-1]) (concat g2D)
+    where g2D  = lines str   
+          n    = length g2D
+          m    = if null g2D then 0 else length (head g2D)
+
+ngbrs :: (Ix i, Enum i, Num i) => Array (i,i) e -> (e -> Bool) -> (i,i) -> Int
+ngbrs arr p (i,j) = length [ () | (rr,cc) <- liftA2 (,) [i-1..i+1] [j-1..j+1]
+                                , inRange (bounds arr) (rr,cc)
+                                , p (arr!(rr,cc))]
+
+
+coords :: (Ix i, Enum i) => Array (i,i) e -> [(i,i)]
+coords arr = liftA2 (,) [r0..rn] [c0..cn] where ((r0,c0),(rn,cn)) = bounds arr
