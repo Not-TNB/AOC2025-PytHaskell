795e9380dfc1374d4c0b9b165879aaab8cc35122
Tristan Basri <tristanbasri@Tristans-MacBook-Pro.local>
2025-12-07 10:01:34 +0000

    Python D7

---
+++ Python/DAY7.py
@@ -0,0 +1,31 @@
+from collections import defaultdict
+from typing import Set, Tuple
+
+with open('External_Files/day7.txt') as f:
+    grid = f.read().splitlines()
+f.close()
+
+def simulate(manifold):
+    h, w = len(manifold), len(manifold[0])
+    beams: Set[Tuple[int,int]] = {start := (grid[0].index('S'),0)} # init with the start beam pos
+    ways = defaultdict(int)
+    ways[start] = 1
+    split = paths = 0
+    while beams:
+        new = set()
+        for (x,y) in beams:
+            if (y1:=y+1)>=h: 
+                paths += ways[(x, y)]
+            elif manifold[y1][x]=='^':
+                split += 1
+                for x1 in (x-1,x+1): 
+                    if 0<=x1<w: 
+                        ways[(x1,y1)] += ways[(x,y)]
+                        new.add((x1,y1))
+            else: 
+                ways[(x,y1)] += ways[(x,y)]
+                new.add((x,y1))
+        beams = new
+    return split, paths
+
+print(simulate(grid)) # PART 1: 1658, PART 2: 53916299384254s
