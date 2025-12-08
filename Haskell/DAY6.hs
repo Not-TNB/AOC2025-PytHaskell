79a0a05489f2570512c2c6ecc860cbffbc95c2f0
Tristan Basri <tristanbasri@Tristans-MacBook-Pro.local>
2025-12-08 00:16:19 +0000

    Shortened Haskell D6

--- Haskell/DAY6.hs
+++ Haskell/DAY6.hs
@@ -12,14 +12,14 @@ import Text.Read (readMaybe)
 main :: IO ()
 main = do
     file <- readFile "../External_Files/day6.txt"
-    print $ part1 file                                             -- PART 1: 5877594983578
+    print $ part1 file                                           -- PART 1: 5877594983578
     print $ evalState (part2 $ cols $ setupGrid file) ('+',[],0) -- PART 2: 11159825706149
 
 part1 :: String -> Int
 part1 = sum . map (calculate . reverse) . transpose . map words . lines
     where calculate ([op]:nums) = apply op (map read nums)
 
---                          op     grp    acc
+--                              op     grp   acc
 part2 :: [(Int,Char)] -> State (Char, [Int], Int) Int
 part2 [] = gets (\(op,grp,acc) -> acc + apply op grp)
 part2 ((num,curOp):cls) = do
@@ -31,5 +31,5 @@ apply :: Char -> ([Int] -> Int)
 apply op = if op == '+' then sum else product
 
 cols :: GridArray -> [(Int,Char)]
-cols arr = mapMaybe (\c -> let s = [arr!(r,c) | r <- [0..n-1]] in (,arr!(n,c)) <$> readMaybe s) [0..m]
+cols arr = mapMaybe (\c -> (,arr!(n,c)) <$> readMaybe [arr!(r,c) | r <- [0..n-1]]) [0..m]
   where (_,(n,m)) = bounds arr
